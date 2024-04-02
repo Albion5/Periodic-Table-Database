37ef8eddf2377bbac8c18a5eb8b79421ae049fcf
Alina Safronova <amifinova@gmail.com>
2024-04-02 13:24:28 +0000

    feat: add find element function

--- element.sh
+++ element.sh
@@ -1,6 +1,25 @@
+PSQL="psql --username=freecodecamp --dbname=periodic_table --no-align --tuples-only -c"
+
+FIND_ELEMENT_BY_NAME() {
+  echo $($PSQL "SELECT atomic_number FROM elements WHERE symbol='$1' OR name='$1'")
+}
+
+FIND_ELEMENT_BY_NUMBER() {
+  echo $($PSQL "SELECT atomic_number FROM elements WHERE atomic_number=$1")
+}
+
+FIND_ELEMENT() {
+  if [[ "$1" =~ ^[0-9]+$ ]]
+  then
+    echo $(FIND_ELEMENT_BY_NUMBER $1)
+  else
+    echo $(FIND_ELEMENT_BY_NAME "$1")
+  fi
+}
 if [[ $# -eq 0 ]]
 then
   echo "Please provide an element as an argument."
 else
-  echo "$1"
+  ELEMENT_INFO="$(FIND_ELEMENT "$1")"
+  echo $ELEMENT_INFO
 fi
