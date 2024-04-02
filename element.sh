0034995be605ab7c4951aa19e347e5e6c3881ec7
Alina Safronova <amifinova@gmail.com>
2024-04-02 14:00:29 +0000

    feat: add output element info function

--- element.sh
+++ element.sh
@@ -8,6 +8,17 @@ FIND_ELEMENT_BY_NUMBER() {
   echo $($PSQL "SELECT atomic_number FROM elements WHERE atomic_number=$1")
 }
 
+GET_ELEMENT_INFO () {
+  echo "$($PSQL "SELECT name, symbol, types.type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE atomic_number=$1")"
+}
+
+OUTPUT_ELEMENT_INFO() {
+  local ATOMIC_NUMBER=$1
+  local ELEMENT_INFO="$(GET_ELEMENT_INFO $ATOMIC_NUMBER)"
+  IFS='|' read -r NAME SYMBOL TYPE MASS MELTING_POINT BOILING_POINT <<< "$ELEMENT_INFO"
+  echo "The element with atomic number $1 is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."  
+}
+
 FIND_ELEMENT() {
   if [[ "$1" =~ ^[0-9]+$ ]]
   then
@@ -20,6 +31,12 @@ if [[ $# -eq 0 ]]
 then
   echo "Please provide an element as an argument."
 else
-  ELEMENT_INFO="$(FIND_ELEMENT "$1")"
-  echo $ELEMENT_INFO
+  ATOMIC_NUMBER="$(FIND_ELEMENT "$1")"
+  if [[ -z $ATOMIC_NUMBER ]]
+  then
+    echo I could not find that element in the database.
+  else
+    OUTPUT_ELEMENT_INFO $ATOMIC_NUMBER
+  fi
+
 fi
